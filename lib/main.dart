8a467f82cd703a96013e7f2b5c72572fa543a7ea
wlsdlwlsdlwl <127748372+wlsdlwlsdlwl@users.noreply.github.com>
2024-12-05 11:04:33 +0900

    알러지 대체

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import 'package:flutter/material.dart';
 
 void main() {
@@ -123,3 +124,145 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+=======
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'screens/HomeScreen.dart';
+import 'screens/FavoriteRecipesScreen.dart';
+import 'screens/SettingScreen.dart';
+import 'providers/AllergyProvider.dart';
+import 'providers/MaterialProvider.dart';
+import 'src/createRecipesFromFiles.dart'; // JSON 생성 함수 파일 추가
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load();  // .env 파일 로드
+  // JSON 생성 후 실행
+  await createRecipesFromFiles(); // JSON 데이터 생성
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AllergyProvider()), // 알러지 상태 관리
+        ChangeNotifierProvider(create: (_) => MaterialProvider()), // 재료 상태 관리
+      ],
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 1; // 초기 화면은 검색 화면
+  List<Map<String, dynamic>> recipes = []; // 레시피 데이터 저장
+  List<String> allIngredients = []; // 전체 재료 데이터 저장
+  late List<Widget> _screens; // 화면 리스트
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData(); // JSON 데이터 불러오기
+  }
+
+  // JSON 데이터를 로드하는 함수
+  Future<void> _loadData() async {
+    try {
+      final file = File('recipes.json'); // JSON 파일 경로
+      if (await file.exists()) {
+        final jsonData = await file.readAsString();
+        final Map<String, dynamic> data = jsonDecode(jsonData);
+
+        setState(() {
+          recipes = List<Map<String, dynamic>>.from(data['recipes']);
+          allIngredients = List<String>.from(data['allIngredients']);
+        });
+
+        // 전역 재료 데이터 초기화
+        final materialProvider = Provider.of<MaterialProvider>(context, listen: false);
+        materialProvider.setMaterials(allIngredients);
+      } else {
+        print('recipes.json not found. Using default recipes.');
+      }
+    } catch (e) {
+      print('Error loading recipes: $e');
+    }
+
+    _initializeScreens();
+  }
+
+  // 각 화면 초기화
+  void _initializeScreens() {
+    _screens = [
+      FavoriteRecipesScreen(
+        recipes: recipes,
+        onUpdate: _updateRecipes,
+      ),
+      HomeScreen(
+        allIngredients: allIngredients, // 전체 재료 리스트 전달
+        recipes: recipes,
+        onUpdate: _updateRecipes,
+      ),
+      SettingsScreen(),
+    ];
+  }
+
+  // 레시피 업데이트 함수
+  void _updateRecipes(List<Map<String, dynamic>> updatedRecipes) {
+    setState(() {
+      recipes = updatedRecipes;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: recipes.isEmpty
+          ? const Center(child: CircularProgressIndicator()) // 로딩 화면
+          : _screens[_currentIndex], // 현재 화면 표시
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        backgroundColor: Colors.green,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.lightGreen[100],
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: '즐겨찾기',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: '검색',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: '설정',
+          ),
+        ],
+      ),
+    );
+  }
+}
+>>>>>>> Stashed changes
---
+++ lib/screens/RecipeDetailScreen.dart
@@ -0,0 +1,248 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/AllergyProvider.dart';
+import '../services/openai_service.dart';
+
+
+class RecipeDetailScreen extends StatelessWidget {
+  final Map<String, dynamic> recipe;
+
+  const RecipeDetailScreen({
+    required this.recipe,
+  });
+
+
+  void _fetchSubstitute(BuildContext context, String ingredient) async {
+    final allergyProvider = Provider.of<AllergyProvider>(context, listen: false);
+    final excludedIngredients = allergyProvider.getExcludedIngredients();
+
+    final openAIService = OpenAIService();
+    // OpenAI API 호출
+    try {
+      final substitute = await openAIService.sendMessage(recipe['title'], ingredient, excludedIngredients);
+      _showSubstituteDialog(context, ingredient, substitute);
+    } catch (error) {
+      _showErrorDialog(context, "대체 재료를 불러오지 못했습니다.");
+    }
+  }
+
+  void _showSubstituteDialog(BuildContext context, String ingredient, String substitute) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('$ingredient 대체재'),
+          content: Text('$ingredient를 대신할 재료로 $substitute을 추천합니다.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('확인'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showErrorDialog(BuildContext context, String error) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('오류'),
+          content: Text('대체 재료를 가져오는 중 문제가 발생했습니다: $error'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('확인'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // 데이터 가져오기
+    final String title = recipe['title'] ?? '레시피 제목 없음';
+    final String imageUrl = recipe['images']?['large'] ?? ''; // 큰 이미지 URL
+    final Map<String, dynamic> nutrition = recipe['nutrition'] ?? {};
+    final List<String> nutritionInfo = [
+      '${nutrition['calories'] ?? 0}kcal 열량',
+      '${nutrition['carbs'] ?? 0}g 탄수화물',
+      '${nutrition['protein'] ?? 0}g 단백질',
+      '${nutrition['fat'] ?? 0}g 지방',
+      '${nutrition['sodium'] ?? 0}mg 나트륨'
+    ];
+    final List<String> detailedIngredients =
+    List<String>.from(recipe['detailedIngredients'] ?? []);
+    final List<String> instructions =
+    List<String>.from(recipe['instructions'] ?? []);
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          title,
+          style: const TextStyle(
+              color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // 레시피 제목
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 16),
+            // 이미지
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Container(
+                height: 200,
+                width: double.infinity,
+                color: Colors.grey[200],
+                child: _isValidNetworkImage(imageUrl)
+                    ? Image.network(
+                  imageUrl,
+                  fit: BoxFit.cover,
+                  errorBuilder: (context, error, stackTrace) =>
+                  const Center(child: Text('이미지 로드 실패')),
+                )
+                    : const Center(
+                  child: Text(
+                    '이미지가 없습니다',
+                    style: TextStyle(color: Colors.grey, fontSize: 18),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 24),
+            // 영양정보
+            const Text(
+              '영양정보',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Wrap(
+              spacing: 12,
+              runSpacing: 12,
+              children: nutritionInfo
+                  .map(
+                    (info) => Chip(
+                  label: Text(
+                    info,
+                    style: const TextStyle(
+                        fontSize: 14, color: Colors.black),
+                  ),
+                  backgroundColor: Colors.green[100],
+                ),
+              )
+                  .toList(),
+            ),
+            const SizedBox(height: 24),
+            // 필요한 재료
+            const Text(
+              '필요한 재료',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 16),
+
+
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: detailedIngredients.map((ingredient) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green[100],
+                      foregroundColor: Colors.black,
+                    ),
+                    onPressed: () async {
+                      // OpenAIService를 사용하여 대체 재료를 가져오는 함수 호출
+                      final openAIService = OpenAIService();
+                      final allergyProvider = Provider.of<AllergyProvider>(context, listen: false);
+                      final excludedIngredients = allergyProvider.getExcludedIngredients();
+                      _fetchSubstitute(context, ingredient);
+                      try {
+                        final substitute = await openAIService.sendMessage(recipe['title'], ingredient, excludedIngredients);
+                        _showSubstituteDialog(context, ingredient, substitute);
+                      } catch (e) {
+                        _showErrorDialog(context, e.toString());
+                      }
+                    },
+                    child: Text(
+                      ingredient,
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
+
+
+
+            const SizedBox(height: 24),
+            // 만드는 법
+            const Text(
+              '만드는 법',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: instructions.map((step) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Text(
+                    step,
+                    style: const TextStyle(fontSize: 16, color: Colors.black87),
+                  ),
+                );
+              }).toList(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  bool _isValidNetworkImage(String url) {
+    // URL이 비어있거나 file://로 시작하면 유효하지 않은 것으로 간주
+    return url.isNotEmpty && Uri.tryParse(url)?.hasAbsolutePath == true;
+  }
+}
